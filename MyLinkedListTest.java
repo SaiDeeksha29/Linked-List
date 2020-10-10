65943a34c0012468311699c16f3056cecd1954c4
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-10 22:29:45 +0530

    Added:Linked List created by appending numbers from last

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -21,4 +21,17 @@ public class MyLinkedListTest {
 		Assert.assertTrue(result);
 	}
 
+	@Test
+	public void given3NumbersAppendedFromLastTest() {
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(firstNode);
+		myLinkedList.add(secondNode);
+		myLinkedList.add(thirdNode);
+		boolean result = myLinkedList.head.equals(thirdNode) && myLinkedList.head.getNext().equals(secondNode)
+				&& myLinkedList.tail.equals(firstNode);
+		Assert.assertTrue(result);
+	}
 }
