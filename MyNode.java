85d670357c0a0bb7971fea0da068f7feaa4651a6
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-10 21:52:19 +0530

    Added: Simple Linked List is created

--- MyNode.java
+++ MyNode.java
@@ -1,1 +1,28 @@
+package com.blz.training;
 
+public class MyNode<K> {
+
+	private K key;
+	private MyNode next;
+	
+	public MyNode(K key) {
+		this.key=key;
+		this.next=null;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public MyNode getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+}
