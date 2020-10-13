f4ac34896c38fb85978b038628ea2bde7c9601d8
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-13 20:05:11 +0530

    Added:Linked list with hashMap is used with hashtables to find the frequency of words

---
+++ MyLinkedHashMapTest.java
@@ -0,0 +1,30 @@
+package com.blz.training;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MyLinkedHashMapTest {
+
+	@Test
+	public void givenASentenceWhenWordsAreAddedToListShouldReturnWordsFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but"
+				        + " because they keep putting themselves deliberately "
+				        + "into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		int frequency = myLinkedHashMap.get("paranoid");
+		Assert.assertEquals(3, frequency);
+	}
+	
+}
